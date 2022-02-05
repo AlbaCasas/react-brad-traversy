2b0bb17319859d3a2de483f81ed087848800c3e1
Alba Casas <albacasasarzua1b@gmail.com>
2022-02-05 17:35:11 +0100

    Complete react crash course from Brad traversy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchAndSetTasks = async () => {
+      const data = await fetchTasks();
+      setTasks(data);
+    };
+
+    fetchAndSetTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5001/task");
+    const data = await res.json();
+    return data;
+  };
+
+  //Add Task
+  const addTask = async (text, day, reminder) => {
+    await fetch("http://localhost:5001/task", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text, day, reminder }),
+    });
+
+    const data = await fetchTasks();
+    setTasks(data);
+  };
+
+  // Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5001/task/${id}`, { method: "DELETE" });
+    const data = await fetchTasks();
+    setTasks(data);
+  };
+
+  // toggle reminder
+  const toggleReminder = async (id) => {
+    const task = tasks.find((task) => task.id === id);
+    await fetch(`http://localhost:5001/task/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...task, reminder: !task.reminder }),
+    });
+    const data = await fetchTasks();
+    setTasks(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to Show"
+      )}
     </div>
   );
 }
